e0edc8456248fbadc713a94caf89e15ff7a3768d
SilverAceLegend <SilverAceLegend@users.noreply.github.com>
2017-05-22 00:54:20 +0200

    initial testing

--- MapProject/jss/app.js
+++ MapProject/jss/app.js
@@ -1,1 +1,15 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
+var home = document.getElementById("home");
+var request= {
+  location: home
+};
+
+
+var ViewModel = {};
