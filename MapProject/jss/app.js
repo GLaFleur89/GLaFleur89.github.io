5ab256787a039956546d94cd7f6776783239e5d7
SilverAceLegend <SilverAceLegend@users.noreply.github.com>
2017-05-22 14:02:25 +0200

    Init Map
    Initiate map based on searched location

--- MapProject/jss/app.js
+++ MapProject/jss/app.js
@@ -1,15 +1,38 @@
 var map;
       function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
+
+
+        var input = document.getElementById("home");
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById("homebutton").addEventListener("click",sethome);
+
+        function sethome() {
+          var home = document.getElementById("home").value;
+          console.log(home);
+
+          if (home == "") {
+            alert("No location selected!");
+          } else {
+            geocodeAddress(geocoder);
+          }
+          function geocodeAddress(geocoder) {
+            var address = document.getElementById("home").value;
+      geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            var baselatlong =(results[0].geometry.location);
+            map = new google.maps.Map(document.getElementById('map'), {
+
+              zoom: 8
+            });
+            map.setCenter(results[0].geometry.location);
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      })};
+        };
+
+
+
       }
-
-var home = document.getElementById("home");
-var request= {
-  location: home
-};
-
-
-var ViewModel = {};
