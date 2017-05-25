17f6517699fd2a5851006c10acb408d64003336a
SilverAceLegend <SilverAceLegend@users.noreply.github.com>
2017-05-25 02:40:29 +0200

    created list
    removed app.js and made one KO view model for everything except initial function.

---
+++ MapProject/jss/ViewModel.js
@@ -0,0 +1,95 @@
+function initMap() {
+  var input = document.getElementById("home");
+  var autocomplete = new google.maps.places.Autocomplete(input);
+};
+
+var myViewModel = function() {
+  this.markerslist = ko.observableArray();
+  var self = this;
+  var map;
+  var markers = [];
+  var labels="123456789";
+  var labelindex = 0;
+
+  this.sethome = function () {
+    var home = document.getElementById("home").value;
+    var geocoder = new google.maps.Geocoder();
+    if (home == "") {
+      alert("No location selected!");
+    } else {
+      self.markerslist.removeAll();
+      geocodeAddress(geocoder);
+
+      }
+  };
+
+  function geocodeAddress(geocoder) {
+    var address = document.getElementById("home").value;
+    geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+    var baselatlong =(results[0].geometry.location);
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15
+    });
+    map.setCenter(results[0].geometry.location);
+    searchplaces(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }})
+  };
+
+  function searchplaces(base) {
+    hidemarkers(markers);
+    var bounds = map.getBounds();
+    var places = new google.maps.places.PlacesService(map);
+    places.nearbySearch({
+      location: base,
+      radius: 1000,
+      keyword: ''
+    }, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+      }else {
+        alert('Markers was not successful for the following reason: ' + status);
+      }});
+  };
+
+  function hidemarkers(marker) {
+    for (i=0; i<marker.length; i++) {
+      markers[i].setMap(null);}
+  }
+
+  function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var points = places.length;
+    if (points < 5) {
+      alert("Only "+points+" points of interest found");
+    };
+    for (var i = 1; i < 6; i++) {
+      var place = places[i];
+      self.markerslist.push({title:place.name,id:i});
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(35, 35),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(15, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        title: place.name,
+        position: place.geometry.location,
+        id: place.id,
+        label: labels[labelindex++]
+      });
+      // If a marker is clicked, do a place details search on it in the next function.
+      marker.addListener('click', function() {
+      getPlacesDetails(this, place);
+      });
+      markers.push(marker);
+    }
+      labelindex=0;
+  }
+
+}
